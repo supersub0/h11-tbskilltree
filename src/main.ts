32e3043a7b64bdd21e209a6b7cba3ffa36835894
Robert Gross <rog@tradebyte.biz>
2018-04-06 12:53:22 +0200

    phonegap/cordova/angular: initial commit.

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+
+// Enable production mode unless running locally.
+if (!/localhost/.test(document.location.host)) {
+  enableProdMode();
+}
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.log(err));
